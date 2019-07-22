62e5fdfd7e858ca4c163f5869bdef6ab83dc673e
Lindsay Berkhout <34925577+lmberkhout@users.noreply.github.com>
2019-07-22 15:12:27 -0700

    Update radiosky.py
    ascii fix

--- generator/radiosky.py
+++ generator/radiosky.py
@@ -31,7 +31,7 @@ def get_map():
     ssbodies = ['mercury', 'venus', 'mars', 'jupiter', 'saturn', 'neptune', 'uranus']
     colors = ['grey', 'pink', 'red', 'orange', 'yellow', 'blue', 'blue', 'blue']
         
-    pic = astropy.coordinates.SkyCoord(ra = '05h19m49.7230919028', dec = '−45° 46′ 44″') #Pictor
+    pic = astropy.coordinates.SkyCoord(ra = '05h19m49.7230919028', dec = '-45d 46m 44s') #Pictor
     forn = astropy.coordinates.SkyCoord(ra = '03h23m25.1s', dec = '-37d 08m')
     cass = astropy.coordinates.SkyCoord(ra = '23h 23m 24s', dec = '+58d 48.9m')
     crab = astropy.coordinates.SkyCoord(ra = '05h 34m 31s', dec = '+22d 00m 52.2s')
