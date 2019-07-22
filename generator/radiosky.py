b10e9440ab0291cf928edf4c1502ef046db8b3dd
Lindsay Berkhout <lindsaymberkhout@gmail.com>
2019-07-22 12:54:53 -0700

    Fixed ascii error

--- generator/radiosky.py
+++ generator/radiosky.py
@@ -1,10 +1,3 @@
-
-# coding: utf-8
-
-# In[32]:
-
-
-get_ipython().run_line_magic('matplotlib', 'notebook')
 import matplotlib.pyplot as plt
 import numpy as np
 import healpy
@@ -15,42 +8,36 @@ from astropy import units as u
 from astropy.io import fits
 from astropy.utils.data import get_pkg_data_filename
 from astropy.time import Time
-
-
-# In[33]:
-
+from datetime import datetime, timedelta
 
 #file can be retrived by running !wget http://danielcjacobs.com/uploads/test4.fits
 mappy = healpy.read_map('test4.fits')
 
-
-# In[34]:
-
-
 def get_map():
     #Get current time 
     now = Time.now()
-    sidereal = now.sidereal_time('mean', longitude =  22.13303)
+    T = datetime.utcnow() + timedelta(hours=2)
+    T  = Time(T, format = 'datetime')
     loc = astropy.coordinates.EarthLocation(lon =  22.13303, lat = -31.58)
-    T = Time(now, format = 'datetime', location = loc)
-   
-    ra = (now.sidereal_time('mean', longitude =  22.13303))/u.hourangle
-    dec = -31.58
-    rot = [-112.13, -31.06]
+    #print(T)
     
-    moon = astropy.coordinates.get_moon(T, location = loc, ephemeris=None, )
+    ra = (T.sidereal_time('mean', longitude =  22.13303))/u.hourangle
+    dec = -31.58
+    rot = [90-ra*15, dec]
+    
+    moon = astropy.coordinates.get_moon(T, location = loc, ephemeris=None)
     sun = astropy.coordinates.get_sun(T)
     
     ssbodies = ['mercury', 'venus', 'mars', 'jupiter', 'saturn', 'neptune', 'uranus']
     colors = ['grey', 'pink', 'red', 'orange', 'yellow', 'blue', 'blue', 'blue']
         
-    pic = astropy.coordinates.SkyCoord(ra = '05h19m49.7230919028', dec = '−45° 46′ 44″') #Pictor
-    forn = astropy.coordinates.SkyCoord(ra = '03h23m25.1s', dec = '-37° 08')
-    cass = astropy.coordinates.SkyCoord(ra = '23h 23m 24s', dec = '+58° 48.9′')
-    crab = astropy.coordinates.SkyCoord(ra = '05h 34m 31s', dec = '+22° 00′ 52.2″')
-    lmc =  astropy.coordinates.SkyCoord(ra = '05h 40m 05s', dec = '−69° 45′ 51″')
-    smc =  astropy.coordinates.SkyCoord(ra = '00h 52m 44.8s', dec = '−72° 49′ 43″')
-    cenA = astropy.coordinates.SkyCoord(ra = '13h 25m 27.6s', dec = '−43° 01′ 09″')
+    pic = astropy.coordinates.SkyCoord(ra = '05h 19m 49.7230919028', dec = '-45d 46m 44s') #Pictor
+    forn = astropy.coordinates.SkyCoord(ra = '03h23m25.1s', dec = '-37d 08m')
+    cass = astropy.coordinates.SkyCoord(ra = '23h 23m 24s', dec = '+58d 48.9m')
+    crab = astropy.coordinates.SkyCoord(ra = '05h 34m 31s', dec = '+22d 00m 52.2s')
+    lmc =  astropy.coordinates.SkyCoord(ra = '05h 40m 05s', dec = '-69d 45m 51s')
+    smc =  astropy.coordinates.SkyCoord(ra = '00h 52m 44.8s', dec = '-72d 49m 43s')
+    cenA = astropy.coordinates.SkyCoord(ra = '13h 25m 27.6s', dec = '-43d 01m 09s')
     callibrator1 =  astropy.coordinates.SkyCoord(ra = 109.32351*u.degree, dec = -25.0817*u.degree)
     callibrator2 = astropy.coordinates.SkyCoord(ra = 30.05044*u.degree, dec = -30.89106*u.degree)
     callibrator3 = astropy.coordinates.SkyCoord(ra = 6.45484*u.degree, dec = -26.0363*u.degree) 
@@ -82,6 +69,7 @@ def get_map():
         healpy.projscatter(body.ra, body.dec, lonlat = True, s = 50, c = colors[count], label = name)
         healpy.projtext(body.ra, body.dec,  lonlat = True, c = 'k', s = name) 
         count += 1
-                                           
+       
+    
 get_map()
-
+plt.savefig('out.png')
