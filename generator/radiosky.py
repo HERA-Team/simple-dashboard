9122825841ac869409e461659e7484923058b2ea
Lindsay Berkhout <lindsaymberkhout@gmail.com>
2019-07-22 14:56:19 -0700

    fixed timezones

--- generator/radiosky.py
+++ generator/radiosky.py
@@ -16,14 +16,14 @@ mappy = healpy.read_map('test4.fits')
 def get_map():
     #Get current time 
     now = Time.now()
-    T = datetime.utcnow() + timedelta(hours=2)
+    T = datetime.utcnow() + timedelta(hours = 0)
     T  = Time(T, format = 'datetime')
     loc = astropy.coordinates.EarthLocation(lon =  22.13303, lat = -31.58)
-    #print(T)
+    print(T)
     
     ra = (T.sidereal_time('mean', longitude =  22.13303))/u.hourangle
-    dec = -31.58
-    rot = [90-ra*15, dec]
+    lon = (ra*15-360)
+    rot = [(lon), -31.58]
     
     moon = astropy.coordinates.get_moon(T, location = loc, ephemeris=None)
     sun = astropy.coordinates.get_sun(T)
@@ -31,13 +31,13 @@ def get_map():
     ssbodies = ['mercury', 'venus', 'mars', 'jupiter', 'saturn', 'neptune', 'uranus']
     colors = ['grey', 'pink', 'red', 'orange', 'yellow', 'blue', 'blue', 'blue']
         
-    pic = astropy.coordinates.SkyCoord(ra = '05h 19m 49.7230919028', dec = '-45d 46m 44s') #Pictor
+    pic = astropy.coordinates.SkyCoord(ra = '05h19m49.7230919028', dec = '−45° 46′ 44″') #Pictor
     forn = astropy.coordinates.SkyCoord(ra = '03h23m25.1s', dec = '-37d 08m')
     cass = astropy.coordinates.SkyCoord(ra = '23h 23m 24s', dec = '+58d 48.9m')
     crab = astropy.coordinates.SkyCoord(ra = '05h 34m 31s', dec = '+22d 00m 52.2s')
-    lmc =  astropy.coordinates.SkyCoord(ra = '05h 40m 05s', dec = '-69d 45m 51s')
-    smc =  astropy.coordinates.SkyCoord(ra = '00h 52m 44.8s', dec = '-72d 49m 43s')
-    cenA = astropy.coordinates.SkyCoord(ra = '13h 25m 27.6s', dec = '-43d 01m 09s')
+    lmc =  astropy.coordinates.SkyCoord(ra = '05h 40m 05s', dec = '−69d 45m 51s')
+    smc =  astropy.coordinates.SkyCoord(ra = '00h 52m 44.8s', dec = '−72d 49m 43s')
+    cenA = astropy.coordinates.SkyCoord(ra = '13h 25m 27.6s', dec = '−43d 01m 09s')
     callibrator1 =  astropy.coordinates.SkyCoord(ra = 109.32351*u.degree, dec = -25.0817*u.degree)
     callibrator2 = astropy.coordinates.SkyCoord(ra = 30.05044*u.degree, dec = -30.89106*u.degree)
     callibrator3 = astropy.coordinates.SkyCoord(ra = 6.45484*u.degree, dec = -26.0363*u.degree) 
@@ -45,7 +45,7 @@ def get_map():
     source_list = [[moon, 'moon', 'slategrey'], [sun, 'sun', 'y'], [pic, 'pictor', 'w'], [forn, 'fornax', 'w'], [cass, 'Cass A', 'w'], [crab, 'Crab', 'w'], 
                    [lmc, 'LMC', 'w'], [cenA, 'Cen A', 'w'], [smc, 'SMC', 'w'], [callibrator1, 'J071717.6-250454', 'r'], [callibrator2, 'J020012.1-305327', 'r'], [callibrator3, ' J002549.1-260210', 'r']]
     
-    healpy.orthview(np.log10(mappy), coord = ['G', 'C'], rot = rot, return_projected_map = True, max = 2, half_sky = 1)
+    healpy.orthview(np.log10(mappy), coord = ['G', 'C'], rot = rot, return_projected_map = True, min = 0, max = 2, half_sky = 1)
     
     
     for item in source_list:
