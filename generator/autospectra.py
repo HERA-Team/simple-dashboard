cc9566d9e9355a0c2cc2d42f61ad9fc3aa360974
Danny Jacobs <daniel.c.jacobs@asu.edu>
2019-08-05 13:33:28 +0200

    comment other refresh command

--- generator/autospectra.py
+++ generator/autospectra.py
@@ -68,7 +68,7 @@ html_preamble = """\
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta http-equiv="refresh" content="60" >
+  <!--- <meta http-equiv="refresh" content="60" > -->
   <title>HERA Auto Spectra Dashboard</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
