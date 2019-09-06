bd7a6e816ec6e9ec445fbedfe4e56800c46852f6
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2019-09-06 13:46:21 -0700

    change table to be given a list of row objects

--- generator/mc_html_summary.py
+++ generator/mc_html_summary.py
@@ -17,6 +17,13 @@ from jinja2 import Environment, FileSystemLoader
 import platform
 
 
+class row(object):
+    def __init__(self, label=None, text=None, color=None):
+        self.label = label
+        self.text = text
+        self.color = color
+
+
 def main():
     if platform.python_version().startswith('3'):
         hostname = os.uname().nodename
@@ -36,11 +43,20 @@ def main():
     # get the most recent observation logged by the correlator
     most_recent_obs = session.get_obs_by_time()[0]
 
+    # load all rows of a table into a list
+    # make rows from the row object
+
+    table = []
+
     dt = (Time.now().gps
           - Time(most_recent_obs.starttime, format='gps', scale='utc').gps
           )
     dt_days = int(floor((dt / 3600.) / 24))
     dt_hours = (dt - dt_days * 3600 * 24) / 3600.
+    last_obs_row = row(label="Time Since Last Obs",
+                       text="{dt_days} days {dt_hours} hours".format(dt_days,
+                                                                     dt_hours))
+    table.append(last_obs_row)
 
     # get the number of raw files in the last 24 hours
     numfiles = (session.query(LibFiles)
@@ -48,6 +64,9 @@ def main():
                         > (Time.now() - TimeDelta(Quantity(1, 'day'))).gps)
                 .filter(LibFiles.filename.like('%uvh5')).count()
                 )
+    nfiles_row = row(label='Raw Files Recorded (last 24 hours)', text=numfiles)
+    table.append(nfiles_row)
+
     # get the number of samples recorded by each node in the last 24 hours
     result = (session.query(NodeSensor.node,
                             func.count(NodeSensor.time))
@@ -58,7 +77,8 @@ def main():
     node_pings = ''
     for l in result:
         node_pings += "Node{node}:{pings}   ".format(node=l[0], pings=l[1])
-
+    ping_row = row(label='Node Sensor Readings (last 24 hours)', text=node_pings)
+    table.append(ping_row)
     # get the current state of is_recording()
     result = (session.query(CorrelatorControlState.state,
                             CorrelatorControlState.time)
@@ -67,15 +87,19 @@ def main():
               )
     is_recording = result[0]
     last_update = Time(result[1], scale='utc', format='gps')
+    on_off_row = row(label='Correlator is')
+    if is_recording:
+        on_off_row.text = "ON"
+        on_off_row.color = 'green'
+    else:
+        on_off_row.text = "OFF"
+        on_off_row.color = None
+    on_off_row.text += "     (last change: {}".format(last_update)
+    table.append(on_off_row)
 
     html_template = env.get_template("mc_table.html")
 
-    rendered_html = html_template.render(dt_days=dt_days,
-                                         dt_hours=int(dt_hours),
-                                         numfiles=numfiles,
-                                         node_pings=node_pings,
-                                         is_recording=is_recording,
-                                         last_update=last_update.iso,
+    rendered_html = html_template.render(table=table,
                                          now=Time.now().iso,
                                          scriptname=os.path.basename(__file__),
                                          hostname=hostname)
