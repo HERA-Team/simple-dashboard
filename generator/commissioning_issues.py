e5268076f17ee3aa3564dec073f2fff37afd15e0
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2019-09-20 09:03:25 -0700

    very basic file creation

---
+++ generator/commissioning_issues.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- mode: python; coding: utf-8 -*-
+# Copyright 2017-2019 the HERA Collaboration
+# Licensed under the 2-clause BSD license.
+
+"""Generate a simple dashboard page displaying issues from HERA-Team/HERA_Commissioning."""
+
+from __future__ import absolute_import, division, print_function
+
+import os
+import sys
+import re
+import redis
+import requests
+from astropy.time import Time
+from jinja2 import Environment, FileSystemLoader
+
+
+def main():
+    pass
+
+
+if __name__ == "main":
+    main()
